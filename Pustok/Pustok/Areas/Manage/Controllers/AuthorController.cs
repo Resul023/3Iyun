bf21642814fe86a526ed67f9ca910034925f039d
Resul023 <73m6u7h@code.edu.az>
2022-06-02 22:59:34 -0700

    HomeWork

---
+++ Pustok/Pustok/Areas/Manage/Controllers/AuthorController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pustok.Areas.Manage.ViewModel;
+using Pustok.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class AuthorController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AuthorController(AppDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            AuthorViewModel authorVW = new AuthorViewModel
+            {
+                Authors = _context.Authors.ToList(),
+            };
+            return View(authorVW);
+        }
+    }
+}
---
+++ Pustok/Pustok/Areas/Manage/Controllers/GenreController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pustok.Areas.Manage.ViewModel;
+using Pustok.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class GenreController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public GenreController(AppDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            GenreViewModel genreVW = new GenreViewModel
+            {
+                Genres = _context.Genres.ToList(),
+            
+            };
+            return View(genreVW);
+        }
+    }
+}
